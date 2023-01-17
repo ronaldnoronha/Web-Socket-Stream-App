354e0701d7ee7962c36efb5d4a5b6c22a1968823
Ronald <rnor3536@uni.sydney.edu.au>
2023-01-17 16:12:07 +1100

    Initial Commit

---
+++ Web Socket Stream App/Web_Socket_Stream_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Web_Socket_Stream_AppApp.swift
+//  Web Socket Stream App
+//
+//  Created by Ronald Noronha on 17/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct Web_Socket_Stream_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
